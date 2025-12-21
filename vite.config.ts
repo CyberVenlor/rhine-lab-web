ed6fde1674873f04af7131d20a3c99a4da68203d
CyberVenlor <badcat17@163.com>
2025-12-22 01:12:23 +0800

    test

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  plugins: [sveltekit()],	
   server: {
     allowedHosts: ['rhine-lab.xyz']
   }
