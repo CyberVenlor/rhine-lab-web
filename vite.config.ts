b5fdc4907cb5699c4a2894096b9e65907b054b9c
CyberVenlor <badcat17@163.com>
2025-12-22 00:57:23 +0800

    allow host

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
-});
+  server: {
+    allowedHosts: ['rhine-lab.xyz']
+  }
+})
